06b07fbd68246395bf961e3333e2f79a89adb69d
Gary <gary.gc.chiu@gmail.com>
2021-11-04 20:58:22 -0400

    attempt at using localstorage

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
 const WATCHED_PERCENTAGE_THRESHOLD = 0;
 const WATCHED_VIDEO_BAR_NODE_NAME = "YTD-THUMBNAIL-OVERLAY-RESUME-PLAYBACK-RENDERER";
 const YOUTUBE_VIDEO_ELEMENT_NODE_NAME = "YTD-RICH-ITEM-RENDERER";
-// const HIDDEN_VIDEOS_STORAGE_KEY = 'hidden-watched-yt-videos';
+const HIDDEN_VIDEOS_STORAGE_KEY = 'hidden-watched-yt-videos';
 
 // callback function to execute when mutations are observed
 const callback = function(mutationsList) {
@@ -12,8 +12,13 @@ const callback = function(mutationsList) {
                 const { watchedPercentage, videoMetadata, videoURL } = getVideoInformation(videoElement);
                 if (watchedPercentage > WATCHED_PERCENTAGE_THRESHOLD) {
                     console.log(`Removing watched video ${videoMetadata} from Recommendations (watched ${watchedPercentage}%). URL: ${videoURL}`);
+                    // update storage
+                    addHiddenVideoToStorage(videoElement);
                     // remove video
                     videoElement.parentElement.removeChild(videoElement);
+                    chrome.storage.local.get(HIDDEN_VIDEOS_STORAGE_KEY, (items) => {
+                        console.log("added? = ", items)
+                    })
                 } else {
                     console.log(`NOT removing video ${videoMetadata}: only ${watchedPercentage}% watched, did not exceed ${WATCHED_PERCENTAGE_THRESHOLD}% threshold`);
                 }
@@ -21,6 +26,25 @@ const callback = function(mutationsList) {
         }
     }
 };
+
+const addHiddenVideoToStorage = (video) => {
+    const { videoMetadata, videoURL } = getVideoInformation(video);
+    // by passing an object you can define default values e.g.: []
+    chrome.storage.local.get({hiddenVideos: []}, function (result) {
+        // the input argument is ALWAYS an object containing the queried keys
+        // so we select the key we need
+        const hiddenVideos = result.hiddenVideos;
+        hiddenVideos.push({videoMetadata});
+        // set the new array value to the same key
+        chrome.storage.local.set({hiddenVideos: hiddenVideos}, function () {
+            // you can use strings instead of objects
+            // if you don't  want to define default values
+            chrome.storage.local.get('hiddenVideos', function (result) {
+                console.log(result.hiddenVideos)
+            });
+        });
+    });
+}
 
 // create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
